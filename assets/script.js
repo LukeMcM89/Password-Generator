a2dcb712b21f6b5e3e3204763308a5774ea386c6
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-15 13:10:18 -0400

    AlertVars

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 // Assignment Code
 document.querySelector("#generate").addEventListener("click", writePassword);
 
+// While I added special symnbols and fullfilled the requirement, I also experimented further with adding emojis just for the fun of it. 
+
 var generateBtn = document.querySelector("#generate");
 var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "|", ".", "_", "/", "`"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "|", ".", "_", "/", "`", "🌹", "💿", "🍕", "🚦", "🚀", "🍩", "🐸", "🦖", "🦕"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
@@ -25,10 +27,16 @@ function generatePassword () {
 
   alert ("Your password will have {confirmLength} characters");
 
-// Write password to the #password input
-
-
-var confirmLowerCase = confirm("Click OK if you want to use lowercase letters");
-var confirmUpperCase = confirm("Click Ok if you want to use uppercase letters");
-var confirmSpecialCharacter = confirm("Click Ok if you want to use special characters");
+var confirmLowerCase = confirm("Click OK if you want to use lowercase letters.");
+var confirmUpperCase = confirm("Click Ok if you want to use uppercase letters.");
+var confirmSpecialCharacter = confirm("Click Ok if you want to use special characters.");
 var confirmNumericCharacter = confirm("Click Ok if you want to use numbers.");
+
+while(confirmLowerCase === false && confirmUpperCase === false && confirmNumericCharacter === false && confirmSPEcialCharacter) {
+  
+  alert("You have to choose one parameter!");
+  var confirmLowerCase = confirm("Click Ok if you want to use lowercase letters.");
+  var confirmUpperCAse = confirm("Click OK if you want to us uppercase letters.");
+  var confirmSpecialCharacter = confirm("Click OK if you want to use special character symbols.");
+  var confirm NumericCharacter = confirm("Click OK if you want to use numbers.");
+} 
