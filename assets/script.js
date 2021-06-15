ff8ed630a73692d421a677c14380053e61e0a266
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-14 23:05:47 -0400

    Confirm Additions

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,22 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var alphaLower = ["abcdefghijklmnopqrstuvwxyz"]
-var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var symbols = ["!@#$%^&*?~|"]
-var numbers = [0123456789]
-function generatePassword () {
 
+
+
+var generateBtn = document.querySelector("#generate");
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!","@", "#", "$", "%", "^", "&", "*", "?", "~", "|", ".", "_", "/", "`"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+var confirmLength = "";
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmSpecialCharacter;
+var NumericaCharacter;
+
+function generatePassword () {
+  var confirmLength = (propmt ("How many characters would you like you password to be?"));
 }
 
 
