058d5e968b6baa2a959739feb7e887e1ecd1346b
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-14 23:42:07 -0400

    Initial Prompts

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 
-
+document.querySelector("#generate").addEventListener("click", writePassword);
 
 var generateBtn = document.querySelector("#generate");
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialChar = ["!","@", "#", "$", "%", "^", "&", "*", "?", "~", "|", ".", "_", "/", "`"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "|", ".", "_", "/", "`"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
@@ -16,10 +16,13 @@ var confirmSpecialCharacter;
 var NumericaCharacter;
 
 function generatePassword () {
-  var confirmLength = (propmt ("How many characters would you like you password to be?"));
+  var confirmLength = (prompt("How many characters would you like you password to be?"));
 }
 
-
+  while(confirmLength <=7 || confirmLength >= 127) {
+    alert("Your password length must be between 8 - 128 characters! Please try again.");
+    var confirmLength = prompt "How many characters would you like your password to have?"
+  }
 
 // Write password to the #password input
 
