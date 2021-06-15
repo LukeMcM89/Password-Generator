863c2ce612241d38576b6a745ebf884d45d068bd
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-14 21:42:18 -0400

    Updated Arrays

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var alphaLower = ["abcdefghijklmnopqrstuvwxyz"]
+var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var symbols = ["!@#$%^&*?~|"]
+var numbers = [0123456789]
+function generatePassword () {
+
+}
+
+
 
 // Write password to the #password input
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
